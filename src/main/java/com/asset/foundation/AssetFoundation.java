dd8b9139cefa8754ccd735ce5e61f74721e3da30
rajesh-198 <59154517+rajesh-198@users.noreply.github.com>
2024-02-02 20:30:43 +1100

    fix

--- src/main/java/com/asset/foundation/AssetFoundation.java
+++ src/main/java/com/asset/foundation/AssetFoundation.java
@@ -17,7 +17,7 @@ public class AssetFoundation {
 
     @Autowired
     UserService userService;
-
+    
     public static void main(String[] args) {
         SpringApplication.run(AssetFoundation.class, args);
     }
--- src/main/java/com/asset/foundation/publication/PublicationController.java
+++ src/main/java/com/asset/foundation/publication/PublicationController.java
@@ -21,20 +21,20 @@ import java.io.IOException;
 
 
 @Controller
-
+@RequestMapping("/publication")
 public class PublicationController {
 
     @Autowired
     private FileStorageService storageService;
 
-    @GetMapping("/publication/list")
+    @GetMapping("/list")
     public String getEventListPage(Model model) {
         model.addAttribute(ParameterConstants.PARAM_PUBLICATION, storageService.findAll());
         return "/publication/list";
     }
 
 
-    @GetMapping("/publication/view/{id}")
+    @GetMapping("/view/{id}")
     public ResponseEntity<Resource> viewPdf(@PathVariable Long id) {
         byte[] pdfData = storageService.getPdfDataById(id);
         if (pdfData != null) {
@@ -49,14 +49,14 @@ public class PublicationController {
         return null;
     }
 
-    @GetMapping("/publication/add")
+    @GetMapping("/add")
     public String getEventAddPage(Model model) {
         model.addAttribute(ParameterConstants.PARAM_ACTION, OperationType.ADD.getValue());
         return "/publication/add";
     }
 
 
-    @PostMapping("/publication/add")
+    @PostMapping("/add")
     public String savePublication(RedirectAttributes redirectAttributes, PublicationDto publicationDto) {
         return storageService.save(redirectAttributes, publicationDto);
     }
